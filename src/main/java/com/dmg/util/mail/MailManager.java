dfb2acdaeab15b25b6f55e111e9a7afcb4657bb0
Mohammad Abdelhadi <m.abdelhadi@gmail.com>
2015-10-12 10:42:57 +0400

    Mail CC

--- src/main/java/com/dmg/util/mail/MailManager.java
+++ src/main/java/com/dmg/util/mail/MailManager.java
@@ -14,14 +14,24 @@ import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.dmg.util.PropertiesManager;
+
 public class MailManager {
 	
 	private static final Logger log = LoggerFactory.getLogger(MailManager.class);
 
-	private static final String SENDER_EMAIL = "dontreply@royalgas.com";
-	private static final String SENDER_USER_NAME = "dontreply@royalgas.com";
-	private static final String SENDER_PASSWORD = "X1Nmdd2T4CbJ";
-	private final String MAIL_SERVER_NAME = "mail.royalgas.com";
+//	private static final String SENDER_EMAIL = "dontreply@royalgas.com";
+//	private static final String SENDER_USER_NAME = "dontreply@royalgas.com";
+//	private static final String SENDER_PASSWORD = "X1Nmdd2T4CbJ";
+//	private final String MAIL_SERVER_NAME = "mail.royalgas.com";
+
+	private static final String SENDER_EMAIL = PropertiesManager.getInstance().getProperty("email.sender.email");
+	private static final String SENDER_USER_NAME = PropertiesManager.getInstance().getProperty("email.semder.username");
+	private static final String SENDER_PASSWORD = PropertiesManager.getInstance().getProperty("email.senderPassword");
+	private static final String MAIL_SERVER_NAME = PropertiesManager.getInstance().getProperty("email.serverName");
+	private static final String MAIL_DEUG = PropertiesManager.getInstance().getProperty("email.debug");
+	private static final String MAIL_SERVER_PORT = PropertiesManager.getInstance().getProperty("email.server.port");
+	
 	
 //	dontreply@royalgas.com
 //	X1Nmdd2T4CbJ
@@ -43,12 +53,12 @@ public class MailManager {
 		Properties props = new Properties();
 		
 		props.put("mail.smtp.host", MAIL_SERVER_NAME);
-		props.put("mail.debug", "true");
+		props.put("mail.debug", MAIL_DEUG);
 		
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		
-		props.put("mail.smtp.port", "26");
+		props.put("mail.smtp.port", MAIL_SERVER_PORT);
 
 		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 			@Override
@@ -61,16 +71,17 @@ public class MailManager {
 
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(SENDER_USER_NAME));
+			message.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(SENDER_EMAIL));
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
 			message.setSubject(subject);
 //			message.setText(body);
 			message.setSentDate(new Date());
 			message.setContent(body, "text/html");
-			log.debug("sent Html Body Message");
+			log.info("sent Html Body Message");
 
 			Transport.send(message);
 
-			System.out.println("Done");
+			log.info("Done");
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
 		}
